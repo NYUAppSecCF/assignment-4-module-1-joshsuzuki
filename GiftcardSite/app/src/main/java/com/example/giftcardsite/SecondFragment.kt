04d7a0278bacde66859e9ff08031f249950e1cf1
joshsuzuki <jks9908@nyu.edu>
2026-04-28 23:18:29 -0400

    fix implicit intent and change github actions to ubuntu-latest

--- GiftcardSite/app/src/main/java/com/example/giftcardsite/SecondFragment.kt
+++ GiftcardSite/app/src/main/java/com/example/giftcardsite/SecondFragment.kt
@@ -1,7 +1,6 @@
 package com.example.giftcardsite
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -66,9 +65,7 @@ class SecondFragment : Fragment() {
                         loggedInUser = response.body()
                         Log.d("Register Success", "Register success. Boo.")
                         Log.d("Register Success", "Token:" + loggedInUser?.token.toString())
-                        var intent = Intent(Intent.ACTION_VIEW)
-                        intent.type = "text/giftcards_browse"
-                        intent.data = Uri.parse("https://appsec.moyix.net/api/index")
+                        var intent = Intent(activity, ProductScrollingActivity::class.java)
                         intent.putExtra("User", loggedInUser);
                         startActivity(intent)
                     }
