db22c58f7d47c7f3434dba1da6c818b8bd45a298
joshsuzuki <jks9908@nyu.edu>
2026-04-29 13:18:12 -0400

    remove extra perms from manifest

--- GiftcardSite/app/src/main/java/com/example/giftcardsite/ProductScrollingActivity.kt
+++ GiftcardSite/app/src/main/java/com/example/giftcardsite/ProductScrollingActivity.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.location.Location
 import android.location.LocationListener
